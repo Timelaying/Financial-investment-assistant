4148f83b1bf8276e7b22568c69e10883d421dab7
tem14mak <tem14@student.le.ac.uk>
2024-03-02 18:46:20 +0000

    minor changes in the login

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import streamlit as st
 from streamlit_login_auth_ui.widgets import __login__
 
 st.set_page_config(
-    page_title="Finance Adviser")
+    page_title="Finance Adviser",
+    page_icon=":moneybag:",
+    layout="wide"
+    )
 
 st.header("Finance Adviser")
 
@@ -10,7 +13,8 @@ __login__obj = __login__(
     auth_token = "pk_prod_0PP3FYA7VXMJ3EKZNB7R7SKWFWHR", # st.secrets.email_api_key
     company_name = "Finance Adviser",
     width = 200, height = 250, 
-    logout_button_name = 'Logout', hide_menu_bool = False, 
+    logout_button_name = 'Logout', 
+    hide_menu_bool = False, 
     hide_footer_bool = False, 
     lottie_url = 'https://assets2.lottiefiles.com/packages/lf20_jcikwtux.json')
 
@@ -18,4 +22,6 @@ LOGGED_IN = __login__obj.build_login_ui()
 
 if LOGGED_IN == True:
 
-    st.markdown("Your Streamlit Application Begins here!")
+   st.success("You are logged in! Welcome to Finance Adviser.")
+else:
+    st.warning("Please log in to access Finance Adviser.")
